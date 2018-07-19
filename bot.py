bf9c0558b5d788158e68b895fb01836abad15556
Junfeng Chen <k-2feng@hotmail.com>
2018-07-19 14:31:36 +0800

    增加时间

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 from wxpy import *
 import time
+import datetime
+
 # 初始化机器人，扫码登陆
 bot = Bot()
 aosong_group = bot.groups().search('澳宋荣光')[0]
 
 while True:
-    aosong_group.send("机器人：群还没沉")
-    time.sleep(60)
-
+    aosong_group.send("姜饼大官人播报：当前%s,群还没沉" % (time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time()))))
+    time.sleep(600)
